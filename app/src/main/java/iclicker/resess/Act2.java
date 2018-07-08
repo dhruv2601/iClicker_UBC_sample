be1056594fc3e40d9fa510504fda2bb508a28c8a
dhruv2601 <dhruvrathi15@gmail.com>
2018-07-07 18:03:05 -0700

    Adding interaction for questionnaire fetch and update

--- app/src/main/java/iclicker/resess/Act2.java
+++ app/src/main/java/iclicker/resess/Act2.java
@@ -9,6 +9,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import mehdi.sakout.fancybuttons.FancyButton;
 
@@ -22,6 +23,11 @@ public class Act2 extends AppCompatActivity {
     private FancyButton btnSubjFour;
 
     private StringRequest stringRequest;
+    private StringRequest stringRequest1;
+    private StringRequest stringRequest2;
+    private StringRequest stringRequest3;
+    private StringRequest stringRequest4;
+
     private String urlUpdateClass = "http://localhost:8000/authenticate.php/";;                 // append url later
     private String urlGetQuestion = "http://localhost:8000/authenticate.php/";                  // append url later
 
@@ -53,6 +59,7 @@ public class Act2 extends AppCompatActivity {
                         Toast.makeText(Act2.this, "Error "+error.getMessage().toString()+" in Act2 update", Toast.LENGTH_SHORT).show();
                     }
                 });
+        Volley.newRequestQueue(this).add(stringRequest);
 
 
         btnSubjOne.setText(courseList[0]);
@@ -63,6 +70,21 @@ public class Act2 extends AppCompatActivity {
         btnSubjOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                urlGetQuestion = urlGetQuestion+"";     // append name of the subject
+                stringRequest1 = new StringRequest(Request.Method.GET,urlGetQuestion ,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(Act2.this, "Act2 updated", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Act2.this, "Error "+error.getMessage().toString()+" in Act2 update", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                Volley.newRequestQueue(Act2.this).add(stringRequest1);
 
             }
         });
@@ -70,6 +92,22 @@ public class Act2 extends AppCompatActivity {
         btnSubjTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                urlGetQuestion = urlGetQuestion+"";     // append name of the subject
+
+                stringRequest2 = new StringRequest(Request.Method.GET,urlGetQuestion ,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // response should contain the next question and answer for next activity
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Act2.this, "Error "+error.getMessage().toString()+" in fetching question ACT2", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                Volley.newRequestQueue(Act2.this).add(stringRequest2);
 
             }
         });
@@ -77,6 +115,22 @@ public class Act2 extends AppCompatActivity {
         btnSubjThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                urlGetQuestion = urlGetQuestion+"";     // append name of the subject
+
+                stringRequest3 = new StringRequest(Request.Method.GET, urlGetQuestion,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // response should contain the next question and answer for next activity
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Act2.this, "Error "+error.getMessage().toString()+" in Act2 update", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                Volley.newRequestQueue(Act2.this).add(stringRequest3);
 
             }
         });
@@ -84,6 +138,22 @@ public class Act2 extends AppCompatActivity {
         btnSubjFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                urlGetQuestion = urlGetQuestion+"";     // append name of the subject
+
+                stringRequest4 = new StringRequest(Request.Method.GET,urlGetQuestion ,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // response should contain the next question and answer for next activity
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Act2.this, "Error "+error.getMessage().toString()+" in Act2 update", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                Volley.newRequestQueue(Act2.this).add(stringRequest4);
 
             }
         });
